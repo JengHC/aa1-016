a7d0088bb9a08b06dd7fda2d6e375e204587a542
jeong hee cheol <70684380+JengHC@users.noreply.github.com>
2020-09-17 16:30:14 +0900

    Add files via upload

--- aa1-16-rpt02/server/http/index2.js
+++ aa1-16-rpt02/server/http/index2.js
@@ -1,19 +1,19 @@
 // http server : index.js
 
-var http = require("");
-port = 3000;
+var http = require("http");
+port = 3003;
 
-var server = http.createServer(function( ) {
-  response.writeHeader( {
+var server = http.createServer((request, response )=> {
+  response.writeHeader(200, {
     "Content-Type": "text/plain"
   });
   response.write("Hello HTTP server from node.js"); // WEB response
   response.write("\n");
-
-  response();
+  response.write("MY ID is aa16!")
+  response.end();
 
 });
 
-server(port);
+server.listen(port);
 console.log("Server Running on " + port + 
 	".\nLaunch http://localhost:" + port);
---
+++ aa1-16-rpt02/start/index_aa16.js
@@ -0,0 +1,4 @@
+var info = require("./AA16info.js");
+
+info("aa16", "Jeong Hea cheol", "010-1234-5678");
+info("aa55", "COMSI","010-1234-5678");
