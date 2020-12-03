3397aa02652a6e944e56cbb306be668073ed04e6
jeong hee cheol <70684380+JengHC@users.noreply.github.com>
2020-12-03 16:47:29 +0900

    Add files via upload

--- aa1-16-rpt10/cds_cht22/cds_dht22_express.js
+++ aa1-16-rpt10/cds_cht22/cds_dht22_express.js
@@ -1,21 +1,23 @@
 // cds_dht22_express.js
-// Express
-
-var express = requre("express");
+var express = require("express");
+var cors = require("cors");
 var app = express();
+app.use(cors());
 var web_port = 3030; // express port
 
 // MongoDB
 var mongoose = require("mongoose");
 var Schema = mongoose.Schema; // Schema object
 // MongoDB connection
-mongoose.connect("mongodb://localhost:27017/iot"); // DB name
+mongoose.connect("mongodb://localhost:27017/iot", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 var db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", function callback() {
   console.log("mongo db connection OK.");
 });
-
 // Schema
 var iotSchema = new Schema({
   date: String,
@@ -25,10 +27,10 @@ var iotSchema = new Schema({
 });
 var Sensor = mongoose.model("Sensor", iotSchema); // sensor data model
 
-// Web routing addrebss
+// Web routing address
 app.get("/", function (req, res) {
   // localhost:3030/
-  res.send("Hello Arduino IOT: express server by AA16!");
+  res.send("Hello Arduino IOT: express server by AA1617!");
 });
 // find all data & return them
 app.get("/iot", function (req, res) {
@@ -44,6 +46,6 @@ app.get("/iot/:id", function (req, res) {
 });
 
 // Express WEB
-app.use(express.static(__dirname + "/public")); // WEB root folder -> public
+app.use(express.static(__dirname + "/public")); // WEB root folder
 app.listen(web_port); // port 3030
-console.log("Express_IOT is running at port:3030");
+console.log("Express_IOT with CORSis running at port:3030");
